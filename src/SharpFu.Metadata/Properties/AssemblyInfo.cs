1486e13e7c1595acba30ecba07f8e3f3a26ff921
xvdiff <xvdiff@gmail.com>
2014-10-13 11:30:07 +0200

    removed metadata project, relocated caching providers

--- src/SharpFu.Metadata/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿#region
-
-using System.Reflection;
-
-#endregion
-
-[assembly: AssemblyTitle("SharpFu Metadata Assembly")]
-[assembly: AssemblyDescription("Library used to provide auto-generated metadata for SharpFu assemblies")]
